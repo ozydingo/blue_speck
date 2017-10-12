1cba6c7a0cb347217dc73dc7e1fa558a8eef6d75
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2017-10-12 13:39:12 -0400

    fix(ActionContrller) [AS] Support mixed despec and despec! by merging params instead of juggling deep dups

--- lib/despecable/action_controller.rb
+++ lib/despecable/action_controller.rb
@@ -1,19 +1,16 @@
 module Despecable
   module ActionController
     def despec(strict: false, &blk)
-      despecable_params = params.deep_dup
-      @despecable_me ||= despecable_me(despecable_params)
-      @despecable_me.doit(strict: strict, &blk)
+      despecable_me(params.deep_dup).doit(strict: strict, &blk)
     end
 
-    def despec!(strict: false, &blk)
-      despecable_params = params
-      @despecable_me ||= despecable_me(despecable_params)
-      @despecable_me.doit(strict: strict, &blk)
+    def despec!(*args, &blk)
+      despec(*args, &blk)
+      params.merge!(despecable_me.params)
     end
 
-    def despecable_me(params)
-      Despecable::Me.new(params, request.query_parameters.merge(request.request_parameters))
+    def despecable_me(params = {})
+      @despecable_me ||= Despecable::Me.new(params, request.query_parameters.merge(request.request_parameters))
     end
   end
 end
