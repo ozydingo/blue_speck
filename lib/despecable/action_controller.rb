b6097c4220911ea785fc3c599aa1b76f2fb5be56
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2017-10-12 12:00:48 -0400

    refactor(Strict) [AS] Move the responsibility of keeping track of spec'd params to the Despecable::Me from the ActionController instance

--- lib/despecable/action_controller.rb
+++ lib/despecable/action_controller.rb
@@ -1,29 +1,19 @@
 module Despecable
   module ActionController
     def despec(strict: false, &blk)
-      me = Despecable::Me.new(params.deep_dup)
-      me.doit(&blk)
-      despecabled.concat(me.specd)
-      despecably_strict if strict
+      despecable_params = params.deep_dup
+      @despecable_me ||= despecable_me(params)
+      @despecable_me.doit(strict: strict, &blk)
     end
 
     def despec!(strict: false, &blk)
-      me = Despecable::Me.new(params)
-      me.doit(&blk)
-      despecabled.concat(me.specd)
-      despecably_strict if strict
+      despecable_params = params
+      @despecable_me ||= despecable_me(params)
+      @despecable_me.doit(strict: strict, &blk)
     end
 
-    def despecabled
-      @despecabled ||= []
-    end
-
-    def despecably_strict
-      unused = request.query_parameters.keys.map(&:to_s) - despecabled.map(&:to_s)
-      if !unused.empty?
-        list = unused.map{|x| "'#{x}'"}.join(", ")
-        raise Despecable::UnrecognizedParameterError, "Unrecognized parameters: #{list}"
-      end
+    def despecable_me(params)
+      Despecable::Me.new(params, request.query_parameters.merge(request.request_parameters))
     end
   end
 end
