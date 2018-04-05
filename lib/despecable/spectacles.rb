71d8420e0fb906905d5e4aafe0f58717148e8003
Andrew Schwartz <andrew@Andrews-MacBook-Pro.local>
2018-04-05 12:59:35 -0400

    feat(String) [AS] Allow case and length options to string params

--- lib/despecable/spectacles.rb
+++ lib/despecable/spectacles.rb
@@ -10,7 +10,7 @@ module Despecable
     end
 
     def read(name, value, type, options)
-      value = public_send(type, name, value) unless value.nil?
+      value = public_send(type, name, value, options) unless value.nil?
       validate_param(name, value, options)
       return value
     end
@@ -25,10 +25,23 @@ module Despecable
     end
 
     def validate_param_value(name, value, allowed_values)
-      msg = "Unacceptable value for parameter: '#{name}'"
-      allowed_values_message = expected_values_message(allowed_values)
-      msg += "; acceptable values are" + allowed_values_message if allowed_values_message.present?
-      raise Despecable::IncorrectParameterError.new(msg, parameters: name) if !allowed_values.include?(value)
+      if !allowed_values.include?(value)
+        msg = "Unacceptable value for parameter: '#{name}'"
+        allowed_values_message = expected_values_message(allowed_values)
+        msg += "; acceptable values are" + allowed_values_message if allowed_values_message.present?
+        raise Despecable::IncorrectParameterError.new(msg, parameters: name)
+      end
+    end
+
+    def validate_string_length(name, value, options)
+      allowed_lengths = options[:length]
+      allowed_lengths = [*allowed_lengths] unless allowed_lengths.is_a?(Range)
+      if !allowed_lengths.include?(value.length)
+        msg = "Unacceptable length for parameter: '#{name}'"
+        allowed_values_message = expected_values_message(allowed_lengths)
+        msg += "; acceptable lengths are" + allowed_values_message if allowed_values_message.present?
+        raise Despecable::IncorrectParameterError.new(msg, parameters: name)
+      end
     end
 
     def expected_values_message(allowed_values)
@@ -44,26 +57,28 @@ module Despecable
       end
     end
 
-    def integer(name, value)
+    def integer(name, value, options)
       Integer(value)
     rescue ArgumentError
       raise unless /^invalid value for Integer/ =~ $!.message
       raise Despecable::InvalidParameterError.new("Integer type required for parameter: '#{name}'", parameters: name)
     end
 
-    def float(name, value)
+    def float(name, value, options)
       Float(value)
     rescue ArgumentError
       raise unless /^invalid value for Float/ =~ $!.message
       raise Despecable::InvalidParameterError.new("Float type required for parameter: '#{name}'", parameters: name)
     end
 
-    def string(name, value)
-      # This is really a no-op.
-      value.to_s
+    def string(name, value, options)
+      value = value.downcase if options[:case] == false
+      value = value.to_s
+      validate_string_length(name, value, options) if options.key?(:length)
+      return value
     end
 
-    def boolean(name, value)
+    def boolean(name, value, options)
       case value.to_s
       when "true", "1" then true
       when "false", "0", nil then false
@@ -71,26 +86,26 @@ module Despecable
       end
     end
 
-    def date(name, value)
+    def date(name, value, options)
       Date.rfc3339(value + "T00:00:00+00:00")
     rescue ArgumentError
       raise unless $!.message == "invalid date"
       raise Despecable::InvalidParameterError.new("Date type (e.g. '2012-12-31') required for parameter: '#{name}'", parameters: name)
     end
 
-    def datetime(name, value)
+    def datetime(name, value, options)
       DateTime.rfc3339(value)
     rescue ArgumentError
       raise unless $!.message == "invalid date"
       raise Despecable::InvalidParameterError.new("Rfc3339 datetime type (e.g. '2009-06-19T00:00:00-04:00') required for parameter: '#{name}'", parameters: name)
     end
 
-    def file(name, value)
+    def file(name, value, options)
       raise Despecable::InvalidParameterError.new("File upload type required for parameter: '#{name}'" , parameters: name) if !(value.respond_to?(:original_filename) && value.original_filename.present?)
       return value
     end
 
-    def any(name, value)
+    def any(name, value, options)
       value
     end
   end
