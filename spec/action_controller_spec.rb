541da3321b59d3c17dc2f0789e472a4684c439a7
Andrew Schwartz <andrew.schwartz@brain-power.com>
2019-03-22 17:47:31 -0400

    Add tests for Despecable::ActionController

---
+++ spec/action_controller_spec.rb
@@ -0,0 +1,64 @@
+require "spec_helper"
+
+# Mock controller for testint
+class Controller
+  include Despecable::ActionController
+  attr_reader :params, :request
+
+  def initialize(params = {})
+    @params = params
+    @request = Request.new(params)
+  end
+end
+
+# Mock request for testing
+class Request
+  attr_reader :params
+  alias_method :request_parameters, :params
+  alias_method :query_parameters, :params
+
+  def initialize(params)
+    @params = params
+  end
+end
+
+describe Despecable::ActionController do
+  describe ".despec" do
+    it "returns but does not overwrite parsed params" do
+      params = {integer: "1", string: "hello"}
+      controller = Controller.new(params)
+      parsed_params = controller.despec do
+        integer :integer
+      end
+      expect(parsed_params[:integer]).to eq(1)
+      expect(params[:integer]).to eq("1")
+      expect(parsed_params[:string]).to eq("hello")
+      expect(params[:string]).to eq("hello")
+    end
+
+    it "barfs in strict mode with unspec'd params" do
+      params = {integer: "1", string: "hello"}
+      controller = Controller.new(params)
+      expect {controller.despec(strict: true) do
+        integer :integer
+      end}.to raise_error do |error|
+        expect(error).to be_a(Despecable::UnrecognizedParameterError)
+        expect(error.parameters).to eq(["string"])
+      end
+    end
+  end
+
+  describe ".despec!" do
+    it "overwrites parsed params" do
+      params = {integer: "1", string: "hello"}
+      controller = Controller.new(params)
+      parsed_params = controller.despec! do
+        integer :integer
+      end
+      expect(parsed_params[:integer]).to eq(1)
+      expect(params[:integer]).to eq(1)
+      expect(parsed_params[:string]).to eq("hello")
+      expect(params[:string]).to eq("hello")
+    end
+  end
+end
